16f1ceb452bfeea7eb6d53417ff3748125f4cd06
Joshua T Corbin <jcorbin@wunjo.org>
2020-10-07 19:05:35 -0700

    refactor the spinner animation a bit

--- index.ts
+++ index.ts
@@ -1,23 +1,33 @@
-const raf = () => new Promise<number>(resolve => requestAnimationFrame(resolve));
+const nextFrame = () => new Promise<number>(resolve => requestAnimationFrame(resolve));
+
+const once = (target:EventTarget, name:string) => new Promise<Event>(resolve => {
+    const handler = (event:Event) => {
+      target.removeEventListener(name, handler);
+      resolve(event);
+    };
+    target.addEventListener(name, handler);
+  }
+);
 
 async function main() {
+  await once(window, 'DOMContentLoaded');
+
   const el = document.querySelector('main');
-  if (!el) return;
+  if (!el) throw new Error('no <main> element');
 
-  let last = await raf();
+  let last = await nextFrame();
   let dt = 0;
-  let elapsed = 0;
+
+  let spin = 0;
   while (true) {
-    elapsed += dt;
-    const n = Math.round(elapsed / 500);
+    spin = (spin + dt / 500) % 5;
 
-    el.innerText = new Array(1 + n % 5).fill('*').join(' ');
+    el.innerText = new Array(1 + Math.round(spin)).fill('*').join(' ');
 
-    const next = await raf();
+    const next = await nextFrame();
     dt = next - last, last = next;
   }
 }
-
-window.addEventListener('DOMContentLoaded', _event => main());
+main();
 
 // vim:set ts=2 sw=2 expandtab:
