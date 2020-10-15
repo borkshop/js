2f464e4ae1000976b316eb69249d3356d2209904
Joshua T Corbin <jcorbin@wunjo.org>
2020-10-14 20:05:00 -0700

    [jspit] breakout input module

---
+++ packages/jspit/src/input.ts
@@ -0,0 +1,73 @@
+export class KeyMap extends Map<string, number> {
+  countKey({altKey, ctrlKey, metaKey, shiftKey, key}:KeyboardEvent) {
+    const name = `${
+      altKey ? 'A-' : ''}${
+      ctrlKey ? 'C-' : ''}${
+      metaKey ? 'M-' : ''}${
+      shiftKey ? 'S-' : ''}${
+      key}`;
+    const n = this.get(name) || 0;
+    this.set(name, n+1);
+  }
+
+  enabled = true
+  filter? : (keyEvent:KeyboardEvent) => boolean
+
+  handleEvent(event:Event) {
+    if (!this.enabled) return;
+    if (event.type !== 'keyup' && event.type !== 'keydown') return;
+    const keyEvent = event as KeyboardEvent;
+    if (this.filter && !this.filter(keyEvent)) return;
+    this.countKey(keyEvent);
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  register(target:EventTarget) {
+    const handler = this.handleEvent.bind(this);
+    target.addEventListener('keydown', handler);
+    target.addEventListener('keyup', handler);
+  }
+
+  consumePresses() {
+    const presses :Array<[string, number]> = [];
+    for (const [name, count] of Array.from(this.entries())) {
+      const n = Math.floor(count / 2);
+      if (n > 0) {
+        const d = count % 2;
+        if (d == 0) this.delete(name);
+        else        this.set(name, 1);
+        presses.push([name, n]);
+      }
+    }
+    return presses;
+  }
+}
+
+function parseMoveKey(key:string) {
+  switch (key) {
+    // arrow keys + stay
+    case 'ArrowUp':    return {x:  0, y: -1};
+    case 'ArrowRight': return {x:  1, y:  0};
+    case 'ArrowDown':  return {x:  0, y:  1};
+    case 'ArrowLeft':  return {x: -1, y:  0};
+    case '.':          return {x:  0, y:  0};
+    default:           return null;
+  }
+}
+
+export function coalesceMoves(presses:Array<[string, number]>) {
+  return presses
+    .map(([key, _count]) => parseMoveKey(key))
+    .reduce((acc, move) => {
+      if (move) {
+        acc.move.x += move.x;
+        acc.move.y += move.y;
+        acc.have = true
+      }
+      return acc;
+    }, {
+      have: false,
+      move: {x: 0, y: 0},
+    });
+}
--- packages/jspit/src/sim.ts
+++ packages/jspit/src/sim.ts
@@ -1,5 +1,6 @@
 import {render, TemplateResult} from 'lit-html';
 import {Point, TileGrid} from './tiles';
+import {KeyMap, coalesceMoves} from './input';
 
 type Nullable<T> = { [P in keyof T]: T[P] | null };
 
@@ -11,85 +12,6 @@ function make(tagName:string, className?:string):HTMLElement {
   const el = document.createElement(tagName);
   if (className) el.className = className;
   return el;
-}
-
-class KeyMap extends Map<string, number> {
-  countKey({altKey, ctrlKey, metaKey, shiftKey, key}:KeyboardEvent) {
-    const name = `${
-      altKey ? 'A-' : ''}${
-      ctrlKey ? 'C-' : ''}${
-      metaKey ? 'M-' : ''}${
-      shiftKey ? 'S-' : ''}${
-      key}`;
-    const n = this.get(name) || 0;
-    this.set(name, n+1);
-  }
-
-  enabled = true
-  filter? : (keyEvent:KeyboardEvent) => boolean
-
-  handleEvent(event:Event) {
-    if (!this.enabled) return;
-    if (event.type !== 'keyup' && event.type !== 'keydown') return;
-    const keyEvent = event as KeyboardEvent;
-    if (this.filter && !this.filter(keyEvent)) return;
-    this.countKey(keyEvent);
-    event.stopPropagation();
-    event.preventDefault();
-  }
-
-  register(target:EventTarget) {
-    const handler = this.handleEvent.bind(this);
-    target.addEventListener('keydown', handler);
-    target.addEventListener('keyup', handler);
-  }
-
-  consumePresses() {
-    const presses :Array<[string, number]> = [];
-    for (const [name, count] of Array.from(this.entries())) {
-      const n = Math.floor(count / 2);
-      if (n > 0) {
-        const d = count % 2;
-        if (d == 0) this.delete(name);
-        else        this.set(name, 1);
-        presses.push([name, n]);
-      }
-    }
-    return presses;
-  }
-}
-
-export interface Move {
-  x: number,
-  y: number,
-}
-
-function parseMoveKey(key:string, _count:number): Move|null {
-  switch (key) {
-    // arrow keys + stay
-    case 'ArrowUp':    return {x:  0, y: -1};
-    case 'ArrowRight': return {x:  1, y:  0};
-    case 'ArrowDown':  return {x:  0, y:  1};
-    case 'ArrowLeft':  return {x: -1, y:  0};
-    case '.':          return {x:  0, y:  0};
-    default:           return null;
-  }
-}
-
-function coalesceKeys(presses:Array<[string, number]>) {
-  return presses
-    .map(([key, count]) => parseMoveKey(key, count))
-    .reduce((acc, move) => {
-      if (move) {
-        acc.move.x += move.x;
-        acc.move.y += move.y;
-        acc.have = true
-      }
-      return acc;
-    }, {
-      have: false,
-      move: {x: 0, y: 0},
-    });
 }
 
 export interface SimAction {
@@ -289,7 +211,7 @@ export class Sim {
     if (movers.length > 1) throw new Error(`ambiguous ${movers.length}-mover situation`);
     const actor = movers[0];
 
-    let {have, move} = coalesceKeys(presses);
+    let {have, move} = coalesceMoves(presses);
     if (!have) return;
 
     const pos = this.grid.getTilePosition(actor);
