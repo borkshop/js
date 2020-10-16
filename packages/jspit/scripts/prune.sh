a05f1926013dfae3e076cf252a12be1fc98052d7
Joshua T Corbin <jcorbin@wunjo.org>
2020-10-15 19:28:30 -0700

    [jspit] add prune script

---
+++ packages/jspit/scripts/prune.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+n=${1:-10}
+
+# ensure the build directory is setup as a secondary working tree
+desc=$(git describe --always)
+build_dir=$(pwd)/build
+git worktree list | grep "$build_dir"
+
+# enter and clean the build tree
+pushd "$build_dir"
+git checkout -f
+git clean -df
+popd
+
+# sort builds descending by build time and prune after the Nth
+builds=$(mktemp)
+find "$build_dir" -type d -d 1 \
+| xargs -n1 basename \
+>"$builds"
+paste \
+  <(<"$builds" xargs git show --no-patch --pretty='format:%ct') \
+  "$builds" \
+| sort -rk1 \
+| tail -n+"$n" \
+| while read t version; do
+
+  # prune a build
+  pushd "$build_dir"
+  git rm -rf "$version"
+  git commit -m "prune $version"
+  popd
+
+done
+
+rm "$builds"
