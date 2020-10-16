65fd19312c1ed83c09200cd344e0271d4eecc7eb
Joshua T Corbin <jcorbin@wunjo.org>
2020-10-15 17:21:23 -0700

    [jspit] add versioned distribution

--- packages/jspit/scripts/dist.sh
+++ packages/jspit/scripts/dist.sh
@@ -5,9 +5,13 @@ desc=$(git describe --always)
 build_dir=$(pwd)/build
 git worktree list | grep "$build_dir"
 
-rm -fv "$build_dir"/* || true
-npm run build
+yarn snowpack build --out "$build_dir/$desc" --clean --sourceMaps
 
 cd "$build_dir"
+
+if prior=$(grep '<meta.*refresh' index.html | grep -o 'url=[^/]*' | cut -d= -f2); then
+    sed -i -e "s/$prior/$desc/g" index.html
+fi
+
 git add -A
 git commit -m "build $desc"
