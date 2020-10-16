8ff4c729cd010ee8158cec857c64cb48e1d0cd7d
Joshua T Corbin <jcorbin@wunjo.org>
2020-10-15 19:11:34 -0700

    [jspit] improve version tagging
    - easily selectable text
    - fixed colorboop binding
    - unify welcome page to use ui module
    - dropped welcome page header/footer in passing, making it more like how
      the demo pages initially load

--- packages/jspit/src/ui.ts
+++ packages/jspit/src/ui.ts
@@ -7,6 +7,19 @@ export interface Bindings {
   version: HTMLElement,
 }
 
+function tailPart(s:string):string {
+  const parts = s.split('/');
+  return parts.pop() || parts.pop() || '';
+}
+
+function getVersion():string {
+  const base = document.querySelector('head base');
+  const version = tailPart(base?.getAttribute('href') || '');
+  if (version && version !== '.') return version;
+  const parts = window.location.pathname.split('/');
+  return parts[parts.length - 2] || '';
+}
+
 export function show(bound:Partial<Bindings>, should:boolean, running:boolean) {
   const overlay = should ? '' : 'none';
   if (bound.head) bound.head.style.display = overlay;
@@ -17,8 +30,5 @@ export function show(bound:Partial<Bindings>, should:boolean, running:boolean) {
     else bound.menu.classList.add('modal');
     bound.menu.style.display = running ? 'none' : '';
   }
-  if (bound.version) {
-    const parts = window.location.pathname.split('/');
-    bound.version.innerText = parts[parts.length - 2] || 'DEV';
-  }
+  if (bound.version) bound.version.innerText = getVersion() || 'DEV';
 }
