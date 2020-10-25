a5138c97b113790d24571f1c23ee338edfba422e
Joshua T Corbin <jcorbin@wunjo.org>
2020-10-25 10:56:21 -0700

    [cdom] convert anim module to ts-checked js

---
+++ packages/cdom/src/anim.js
@@ -0,0 +1,56 @@
+// @ts-check
+
+/**
+ * @return {Promise<number>} - resolves to the current high-res time at the
+ * start of the next browser animation frame
+ */
+export function nextFrame() {
+  return new Promise(resolve => requestAnimationFrame(resolve));
+}
+
+/**
+ * @callback Part - an animation part: a function of elapsed time
+ * @param {number} dt - the amount of time elapsed since the last time this callback was called
+ * @return {boolean} - true if the animation loop should continue, false to stop it
+ */
+
+/**
+ * Runs an animate-able function until it stops (returns false).
+ * @param {Part} update - an animation function
+ */
+export async function everyFrame(update) {
+  let last = await nextFrame();
+  let dt = 0;
+  while (update(dt)) {
+    const next = await nextFrame();
+    dt = next - last, last = next;
+  }
+}
+
+/**
+ * @typedef {Object} TickedPart - an animation part that runs on an internal unit of time ticks
+ * @prop {number} every - specified time tick unit
+ * @prop {Part} then - part callback, will be passed an integer number of elapsed ticks
+ */
+
+/**
+ * @param {(Part|TickedPart)[]} parts - one or more animation parts, optionally with their own internal tic krate
+ * @return {Part} - a compound part that will run all of the given parts
+ */
+export function schedule(...parts) {
+  if (!parts.length) return () => false;
+  const every = parts.map(part => typeof part === 'function' ? 0    : part.every);
+  const then  = parts.map(part => typeof part === 'function' ? part : part.then);
+  const last  = every.map(()   => 0);
+  return (dt) => {
+    for (let i = 0; i < every.length; ++i) {
+      let n = dt;
+      if (every[i] > 0) {
+        last[i] += dt / every[i];
+        if (n = Math.floor(last[i])) last[i] -= n;
+      }
+      if (n && !then[i](n)) return false;
+    }
+    return true;
+  }
+}
