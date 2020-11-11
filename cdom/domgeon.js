f723ad87f1e82fe8795c321476df82b1495e81c8
Joshua T Corbin <jcorbin@wunjo.org>
2020-11-11 12:18:25 -0800

    [cdom/domgeon] fix plane light skip comment

--- cdom/domgeon.js
+++ cdom/domgeon.js
@@ -639,9 +639,10 @@ export class DOMgeon extends EventTarget {
       // clear prior light
       scheme.clear();
 
-      // skip plane if its lightScale isn't below threshold; this happens on
-      // the last tick of the fade out animation, and makes it so that we leave
-      // the light values cleared within a just-exited plane
+      // skip plane if its lightScale is below threshold; this happens
+      // on the last tick of the fade out animation, and makes it so
+      // that we leave the light values cleared within a just-exited
+      // plane
       if (litPlane.lightScale < scheme.lightLimit) continue;
 
       // add ambient light
