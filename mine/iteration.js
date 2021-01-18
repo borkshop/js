390889f7ace20d95e066ae7bd73afe17b2252bf7
Kris Kowal <kris@cixar.com>
2021-01-17 19:56:25 -0800

    [mine/iteration] flesh out count and select

--- mine/iteration.js
+++ mine/iteration.js
@@ -1,23 +1,30 @@
 // @ts-check
 
 /**
- * @param {number} n
+ * Enumerates a range of numbers.
+ *
+ * @param {number} [end]
+ * @param {number} [start]
+ * @param {number} [stride]
  * @yields {number}
  */
-export function *count(n) {
-  for (let i = 0; i < n; i++) {
-    yield i;
+export function *count(end = Infinity, start = 0, stride = 1) {
+  for (; start < end; start += stride) {
+   yield start;
   }
 }
 
 /**
- * @param {Iterable<number>} source
+ * @template T
+ * Enumerates the indexes of the truthy values in the source.
+ * @param {Iterable<T>} source
+ * @param {(value: T) => boolean} [test]
  * @yields {number}
  */
-export function *select(source) {
+export function *select(source, test = Boolean) {
   let i = 0;
   for (const value of source) {
-    if (value) {
+    if (test(value)) {
       yield i;
     }
     i++;
