3fb9de3c2c3c1f12309a389712cb71557e532e22
Joshua T Corbin <jcorbin@wunjo.org>
2021-02-08 12:14:11 -0800

    [idle] add clicker.Sched

---
+++ idle/clicker.js
@@ -0,0 +1,56 @@
+// @ts-check
+
+/** Sched is a delay sleeping and function calling machine.
+  * It always sleeps at least 0 between each function.
+  * Functions are invoked with the schedule object as this.
+  */
+class Sched {
+  /** @param {(number|function)[]} steps */
+  constructor(...steps) {
+    this.steps = steps;
+  }
+
+  start() { if (!this._timer) this.next() }
+  stop() {
+    if (this._timer !== null) clearTimeout(this._timer);
+    this._timer = null;
+  }
+
+  /** @type {null|ReturnType<setTimeout>} */
+  _timer = null;
+  next() {
+    this.stop();
+    let delay = this.step();
+    this._timer = setTimeout(() => this.next(), delay);
+  }
+
+  i = 0;
+  step() {
+    const i = this.i++ % this.steps.length;
+    const part = this.steps[i];
+    switch (typeof part) {
+      case 'function': part.call(this); return 0;
+      case 'number':                    return part;
+      default: throw new Error(`invalid sched.steps[${i}] type ${typeof part}`);
+    }
+  }
+
+  /** afterNext blocks until all current steps have been executed */
+  async afterNext() {
+    return new Promise(resolve => {
+      // TODO reject() if step throws?
+      const i = this.steps.length;
+      this.steps.push(() => {
+        resolve();
+        this.steps.splice(i, 1);
+      })
+    });
+  }
+
+  /** finish waits for all steps to complete, and then stops execution */
+  async finish() {
+    await this.afterNext();
+    this.stop();
+    this.i = 0;
+  }
+}
