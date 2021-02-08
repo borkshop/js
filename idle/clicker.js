901ff57c249525e5d1512fef73afeb90d9f742ac
Joshua T Corbin <jcorbin@wunjo.org>
2021-02-08 14:50:10 -0800

    [idle] add Recorder

--- idle/clicker.js
+++ idle/clicker.js
@@ -72,3 +72,149 @@ function clickit(el, ...every) {
   it.start();
   return it;
 }
+
+/** Recorder supports recording DOM events under some element.
+ */
+class Recorder {
+  /** @type {Element} */
+  under
+
+  /** until determines when recording stops.
+   * The trigerring event is ignored, but a time is still recorded.
+   * May be overridden by constructor option.
+   *
+   * @param {Event} ev
+   * @returns {boolean}
+   */
+  until(ev) {
+    return ev instanceof KeyboardEvent && ev.type === 'keydown' && ev.key === 'Escape';
+  }
+
+  /** ignore determines events elide from recording.
+   * May be overridden by constructor option.
+   *
+   * @param {Event} ev
+   * @returns {boolean}
+   */
+  ignore(ev) {
+    const {type} = ev
+    return type !== 'click' && type !== 'keypress';
+  }
+
+  /** event types to start listening to.
+   * May be overridden by constructor option.
+   */
+  types = [
+    'click',
+    'keydown', // needed to recognize terminal <Escape> key
+    'keypress',
+  ]
+
+  /**
+   * @typedef {object} RecorderOptions
+   * @prop {(ev: Event) => boolean} [until]
+   * @prop {(ev: Event) => boolean} [ignore]
+   * @prop {string[]} [types]
+   */
+
+  /**
+   * @param {Element} under
+   * @param {RecorderOptions} [options]
+   */
+  constructor(under, options) {
+    this.under = under;
+    if (options?.until) this.until = options.until;
+    if (options?.ignore) this.ignore = options.ignore;
+    if (options?.types) this.types = options.types;
+  }
+
+  now() {
+    return this.under.ownerDocument?.defaultView?.performance.now() || 0;
+  }
+
+  /** @type {Set<string>} */
+  listening = new Set()
+
+  started = 0
+
+  start() {
+    for (const type of this.types) {
+      this.under.addEventListener(type, this, {capture: true});
+      this.listening.add(type);
+    }
+    this.started = this.now();
+    this.times = [];
+    this.events = [];
+  }
+
+  stop() {
+    for (const type of this.listening)
+      this.under.removeEventListener(type, this);
+    this.listening.clear();
+  }
+
+  /** @type {number[]} */
+  times = []
+
+  /** @type {(null|Event)[]} */
+  events = []
+
+  /** @type {null|((rec: Recorder) => void)} */
+  ondone = null
+
+  /** @param {Event} ev */
+  handleEvent(ev) {
+    const time = this.now();
+    if (this.until(ev)) {
+      this.times.push(time);
+      this.events.push(null);
+      ev.stopPropagation();
+      ev.preventDefault();
+      this.stop();
+      if (this.ondone) this.ondone(this)
+    } else if (!this.ignore(ev)) {
+      this.times.push(time);
+      this.events.push(ev);
+    }
+  }
+
+  /** @param {(number|function)[]} steps */
+  addSteps(steps) {
+    let last = this.started;
+    for (let i=0; i<this.times.length; i++) {
+      const time = this.times[i];
+      const delay = time - last;
+      last = time;
+      steps.push(delay);
+
+      const ev = this.events[i];
+      if (!ev) return;
+
+      const {target, type, ...init} = ev;
+      if (!target) return;
+
+      const cons = /** @type {{new(type: string, init: any) : Event}} */ (ev.constructor);
+      steps.push(() => target.dispatchEvent(new cons(type, init)));
+    }
+  }
+
+  /** @returns {(number|function)[]} */
+  steps() {
+    /** @type {(number|function)[]} steps */
+    const steps = [];
+    this.addSteps(steps);
+    return steps;
+  }
+}
+
+/**
+ * @param {Element} under
+ * @param {RecorderOptions} [options]
+ */
+function record(under, options={}) {
+  return new Promise(resolve => {
+    const rec = new Recorder(under, options);
+    rec.ondone = () => resolve(rec);
+    rec.start();
+  });
+}
