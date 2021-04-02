4bca020e819ac9e5595fc617a52893cef42ef20f
Joshua T Corbin <jcorbin@wunjo.org>
2021-04-02 11:41:53 -0700

    tarot: break index.{css and js out of html}

---
+++ tarot/index.js
@@ -0,0 +1,87 @@
+// @ts-check
+
+/**
+ * @typedef {Object} Card
+ * @property {string} id
+ * @property {string} title
+ */
+
+export const smithDeck = {
+  cards: [
+    {id: 'ar00', title: 'The Fool'},
+    {id: 'ar01', title: 'The Magician'},
+    {id: 'ar02', title: 'The High Priestess'},
+    {id: 'ar03', title: 'The Empress'},
+    {id: 'ar04', title: 'The Emperor'},
+    {id: 'ar05', title: 'The Hierophant'},
+    {id: 'ar06', title: 'The Lovers'},
+    {id: 'ar07', title: 'The Chariot'},
+    {id: 'ar08', title: 'Strength'},
+    {id: 'ar09', title: 'The Hermit'},
+    {id: 'ar10', title: 'Wheel of Fortune'},
+    {id: 'ar11', title: 'Justice'},
+    {id: 'ar12', title: 'The Hanged Man'},
+    {id: 'ar13', title: 'Death'},
+    {id: 'ar14', title: 'Temperance'},
+    {id: 'ar15', title: 'The Devil'},
+    {id: 'ar16', title: 'The Tower'},
+    {id: 'ar17', title: 'The Star'},
+    {id: 'ar18', title: 'The Moon'},
+    {id: 'ar19', title: 'The Sun'},
+    {id: 'ar20', title: 'Judgement'},
+    {id: 'ar21', title: 'The World'},
+  ].concat(...[
+    ['cu', `Cups`],
+    ['pe', 'Pentacles'],
+    ['sw', `Swords`],
+    ['wa', 'Cups'],
+  ].map(([idSpace, suit]) => [
+    {id: `${idSpace}ac`, title: `Ace of ${suit}`},
+    {id: `${idSpace}02`, title: `Two of ${suit}`},
+    {id: `${idSpace}03`, title: `Three of ${suit}`},
+    {id: `${idSpace}04`, title: `Four of ${suit}`},
+    {id: `${idSpace}05`, title: `Five of ${suit}`},
+    {id: `${idSpace}06`, title: `Six of ${suit}`},
+    {id: `${idSpace}07`, title: `Seven of ${suit}`},
+    {id: `${idSpace}08`, title: `Eight of ${suit}`},
+    {id: `${idSpace}09`, title: `Nine of ${suit}`},
+    {id: `${idSpace}10`, title: `Ten of ${suit}`},
+    {id: `${idSpace}pa`, title: `Page of ${suit}`},
+    {id: `${idSpace}kn`, title: `Knight of ${suit}`},
+    {id: `${idSpace}qu`, title: `Queen of ${suit}`},
+    {id: `${idSpace}ki`, title: `King of ${suit}`},
+  ])),
+  className: 'rws',
+  /** @param {Card} card */
+  buildCardFront: ({id, title}) => `<img class="card-front" draggable="false" alt="${title}" src="www.sacred-texts.com/tarot/pkt/img/${id}.jpg">`,
+  buildCardBack: () => '<img class="card-back" draggable="false" src="www.sacred-texts.com/tarot/pkt/img/verso.jpg">',
+  // icon: `www.sacred-texts.com/tarot/pkt/tn/${id}.jpg`,
+};
+
+/**
+ * @typedef {object} Deck
+ * @property {Card[]} cards
+ * @property {string} className
+ * @property {(card: Card) => string} buildCardFront
+ * @property {(card: Card) => string} buildCardBack
+ * @property {(card: Card, i: number, cards: Card[]) => string} buildCard
+ */
+
+/**
+ * @param {Deck} deck
+ */
+export function buildCards({
+  cards,
+  className,
+  buildCardFront=() => '<div class="card-front"></div>',
+  buildCardBack=() => '<div class="card-back"></div>',
+  buildCard=(card, i, cards) => `
+    <div class="card ${className} ${className}-${card.id}" style="--flip: 0deg; z-index: ${cards.length - i++}">
+      ${buildCardFront(card)}${buildCardBack(card)}
+    </div>
+  `,
+}) {
+  return `<div class="card-pile">${
+    cards.map(buildCard).join('')
+  }</div>`;
+}
