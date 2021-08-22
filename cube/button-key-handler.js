46db4c1ce34ae1b92d81cc84100b8ad435c605d8
Kris Kowal <kris@cixar.com>
2021-08-21 20:48:24 -0700

    [cube] refine button command mapping

--- cube/button-key-handler.js
+++ cube/button-key-handler.js
@@ -24,62 +24,58 @@ export function makeButtonKeyHandler(command) {
   const handler = event => {
     const {key, repeat, metaKey} = event;
     if (repeat || metaKey) return;
+
     switch (key) {
       case '7': // numpad
       case 'u': // hjkl
-      case 'q': // asdf
         command(7);
         break;
 
       case 'ArrowUp':
       case '8': // numpad
       case 'k': // hjkl
-      case 'w': // wasd
+      case 'i': // hjkl
         command(8);
         break;
 
       case '9': // numpad
-      case 'i': // hjkl
-      case 'e': // wasd
+      case 'o':
         command(9);
         break;
 
       case 'ArrowLeft':
       case '4': // numpad
       case 'h': // hjkl
-      case 'a': // wasd
         command(4);
         break;
 
       case '5': // numpad
-      case '.': // hjkl
-      case 'f': // wasd
+      case ' ':
         command(5);
         break;
 
       case 'ArrowRight':
       case '6': // numpad
       case 'l': // hjkl
-      case 'd': // wasd
         command(6);
         break;
 
       case '1': // numpad
       case 'm': // hjkl
-      case 'x': // wasd
+      case 'd':
         command(1);
         break;
 
       case 'ArrowDown':
       case '2': // numpad
       case 'j': // hjkl
-      case 's': // wasd
+      case ',':
         command(2);
         break;
 
       case '3': // numpad
-      case ',': // hjkl
-      case 'c': // wasd
+      case '.': // hjkl
+      case 'f':
         command(3);
         break;
 
