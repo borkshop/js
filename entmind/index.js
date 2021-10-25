2e2b413d2e4d171350dba4ce380b1316d35eb59d
Joshua T Corbin <jcorbin@wunjo.org>
2021-10-25 15:45:22 -0400

    [entmind] iterate on framework, thru loop/sub with full expr types

--- entmind/index.js
+++ entmind/index.js
@@ -1,1 +1,5 @@
-// TODO implement see index.d.ts for definitions and docs so far
+// see index.d.ts for definitions and the bulk of documentation
+
+// TODO basic executors for "first until completion" and "fair roundrobin"
+// TODO common evaluator: boolean, relation, arithmetic, scope
+// TODO common task dispatcher: sleep, timeout, deadline, while/until, sub, scope
