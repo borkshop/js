4da7a4ddbc76a9aba1b6a4ba61f68f121b75822f
Kris Kowal <kris@cixar.com>
2022-04-10 21:55:50 -0700

    [cube] add vector2d module

---
+++ cube/vector2d.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+/**
+ * @typedef {{
+ *   x: number,
+ *   y: number,
+ * }} Point
+ */
+
+/**
+ * @param {Point} a
+ * @param {Point} b
+ */
+export const add = (a, b) => {
+  return {
+    x: a.x + b.x,
+    y: a.y + b.y,
+  };
+};
+
+/**
+ * @param {Point} vector
+ * @param {number} factor
+ */
+export const scale = (vector, factor) => {
+  return {
+    x: vector.x * factor,
+    y: vector.y * factor,
+  };
+};
